7006aa8f8aa89c94a2d6709027a8df5790b9a1d4
kwonsoojeong <0401tnwjd@gmail.com>
2022-01-08 17:32:08 +0900

    naver map api 설정 및 마커 테스트

--- app/src/main/java/com/crystal/airbnb/MainActivity.kt
+++ app/src/main/java/com/crystal/airbnb/MainActivity.kt
@@ -2,10 +2,104 @@ package com.crystal.airbnb
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.naver.maps.geometry.LatLng
+import com.naver.maps.map.*
+import com.naver.maps.map.overlay.Marker
+import com.naver.maps.map.util.FusedLocationSource
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback{
+
+    private val mapView :MapView by lazy{
+        findViewById(R.id.mapView)
+    }
+    private lateinit var naverMap: NaverMap
+    private lateinit var locationSource: FusedLocationSource
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mapView.onCreate(savedInstanceState)
+        mapView.getMapAsync(this)
     }
+
+    override fun onStart() {
+        super.onStart()
+        mapView.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView.onSaveInstanceState(outState)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
+    }
+
+    override fun onMapReady(map: NaverMap) {
+        this.naverMap = map
+        map.maxZoom = 18.0
+        map.minZoom = 10.0
+
+        //지도 위치 설청
+        val cameraUpdate = CameraUpdate.scrollTo(LatLng(37.4977876, 127.0271162))
+        map.moveCamera(cameraUpdate)
+
+        //버튼 클릭 엑션을 하기위해선 권한 필요
+        locationSource = FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE)
+        map.locationSource = locationSource
+
+        //현위치 버튼 활성화
+        map.uiSettings.isLocationButtonEnabled = true
+
+        val marker = Marker()
+        marker.position = LatLng(37.5670135, 126.9783740)
+        marker.map = naverMap
+        //marker.map = null // 마커 제거
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if(requestCode != LOCATION_PERMISSION_REQUEST_CODE){
+            return
+        }
+
+        if(locationSource.onRequestPermissionsResult(requestCode, permissions, grantResults)){
+            if(!locationSource.isActivated){
+                naverMap.locationTrackingMode = LocationTrackingMode.None
+            }
+            return
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    companion object {
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 1000
+    }
+
 }
